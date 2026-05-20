1a803d15c86bbeae37701dd4cef798d9a2fe4c6c
Jasin <piotr.jasinski2002@gmail.com>
2026-05-20 23:02:16 +0200

    PO UCZENIU version 1 params

--- src/train.py
+++ src/train.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
     MAX_PROMPT_LEN = 96
     MAX_CODE_LEN = 512
     BATCH_SIZE = 32
-    NUM_WORKERS = 0
+    NUM_WORKERS = 3
     EPOCHS = 20
     HIDDEN_DIM = 256
     NUM_BLOCKS = 3
