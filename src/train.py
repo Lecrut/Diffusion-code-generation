e56ce9fc8e47da869b4838515258cba5d7eeb876
Jasin <piotr.jasinski2002@gmail.com>
2026-05-20 23:36:29 +0200

    test

--- src/train.py
+++ src/train.py
@@ -239,11 +239,11 @@ if __name__ == "__main__":
     
     MAX_PROMPT_LEN = 96
     MAX_CODE_LEN = 512
-    BATCH_SIZE = 64
+    BATCH_SIZE = 32
     NUM_WORKERS = 3
-    EPOCHS = 20
+    EPOCHS = 100
     HIDDEN_DIM = 256
-    NUM_BLOCKS = 5
+    NUM_BLOCKS = 4
     DATASET_FRACTION = float(os.getenv("DATASET_FRACTION", "1.0")) # size of dataset
 
     tokenizer = CodeTokenizer()
