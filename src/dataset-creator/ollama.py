f5694c8a11cdbe4950e30ea4574eeecd53534396
Lecrut <filip.lecrut12@gmail.com>
2026-06-16 11:33:50 +0200

    new ollama model

--- src/dataset-creator/ollama.py
+++ src/dataset-creator/ollama.py
@@ -8,7 +8,8 @@ from threading import Lock
 
 
 OLLAMA_URL = "http://localhost:11434"
-MODEL = "gemma4:e2b" 
+# MODEL = "gemma4:e2b" - Default model 
+MODEL = "qwen3.5:4b"
 # MODEL = "mistral-medium-3.5:128b" - TO CHANGE FOR TESTING
 
 DATA_DIR = "data"
